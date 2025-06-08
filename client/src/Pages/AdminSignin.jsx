3efcbc503ebcce3769644c333cdc3ebf870befeb
utsab-ad <utasbadhikari075@gmail.com>
2025-06-08 09:33:58 -0600

    updated 001

--- client/src/Pages/AdminSignin.jsx
+++ client/src/Pages/AdminSignin.jsx
@@ -4,7 +4,7 @@ import { LoginForm } from "@/components/login-form";
 const AdminSignin = () => {
   return (
     <div className="flex flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
+      <div className="w-full pt-6 max-w-sm md:max-w-3xl">
         <LoginForm />
       </div>
     </div>
