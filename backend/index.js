ec6b4b85fff7793f66c14007215e540d0eac196e
utsab-ad <utasbadhikari075@gmail.com>
2025-06-08 07:43:49 -0600

    updated 03

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const allowedOrigins = [process.env.FRONTEND_URL, "https://authsecure-mu.vercel.app"];
+const allowedOrigins = [process.env.FRONTEND_URL, 
+  "https://authsecure-mu.vercel.app", 
+  "https://utsabadhikari.name.np"];
 
 app.use(cors({
   origin: function (origin, callback) {
