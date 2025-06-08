fdb6e1cbeb29df0a03abe382e3c77f36c3083974
utsab-ad <utasbadhikari075@gmail.com>
2025-06-08 09:19:44 -0600

    updated 001

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import { RouteChatbot, RouteDocs, RouteIndex } from "./helpers/RouteNames";
+import { RouteAdminSignin, RouteChatbot, RouteDocs, RouteIndex } from "./helpers/RouteNames";
 import Index from "./Pages/Index";
 import Chatbot from "./Pages/chatbot/Chatbot";
 import Layout from "./Pages/Layout/Layout";
 import Docs from "./Pages/documentation/Docs";
+import AdminSignin from "./Pages/AdminSignin";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route index element={<Index />}></Route>
         <Route path={RouteChatbot} element={<Chatbot />}></Route>
         <Route path={RouteDocs} element={<Docs/>}></Route>
+        <Route path={RouteAdminSignin} element={<AdminSignin/>}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/Pages/Layout/AppSidebar.jsx
+++ client/src/Pages/Layout/AppSidebar.jsx
@@ -18,79 +18,84 @@ import { FiUsers } from "react-icons/fi";
 import { GoDot } from "react-icons/go";
 import { FcAbout } from "react-icons/fc";
 import { GrProjects } from "react-icons/gr";
-import { RouteDocs, RouteIndex } from "@/helpers/RouteNames";
+import { RouteAdminSignin, RouteDocs, RouteIndex } from "@/helpers/RouteNames";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { IoIosLogIn } from "react-icons/io";
 
 const AppSidebar = () => {
   return (
-      <Sidebar>
-        <SidebarContent className="bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 text-white transition-transform duration-300 ease-in-out">
-          <SidebarHeader className="flex justify-center items-end">
+    <Sidebar>
+      <SidebarContent className="bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 text-white transition-transform duration-300 ease-in-out">
+        <SidebarHeader className="flex flex-row justify-between items-center">
+          <Link to={RouteAdminSignin}>
+            <Button variant="ghost" size="icon">
+              <IoIosLogIn />
+            </Button>
+          </Link>
+          <SidebarTrigger className="md:hidden" />
+        </SidebarHeader>
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem className="w-full flex flex-col justify-center items-center">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <h1>Utsab Adhikari</h1>
+            </SidebarMenuItem>
+            <Link to={RouteIndex}>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <IoHomeOutline className="cursor-pointer" />
+                  <p className="cursor-pointer w-full">Home</p>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </Link>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <FaBlog /> <Link>Blogs</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <GrProjects />
+                <Link>Projects</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <BiCategoryAlt /> <Link>Categories</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <FiUsers />
+                <Link>Users</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
 
-    <SidebarTrigger className="md:hidden"/>
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <GoDot />
+                <Link>new cate</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
 
-          </SidebarHeader>
-          <SidebarGroup>
-            <SidebarMenu>
-              <SidebarMenuItem className="w-full flex flex-col justify-center items-center">
-                <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <h1>Utsab Adhikari</h1>
-              </SidebarMenuItem>
-              <Link to={RouteIndex}>
-                <SidebarMenuItem>
-                  <SidebarMenuButton>
-                    <IoHomeOutline className="cursor-pointer" />
-                    <p className="cursor-pointer w-full">Home</p>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              </Link>
-              <SidebarMenuItem>
-                <SidebarMenuButton>
-                  <FaBlog /> <Link>Blogs</Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton>
-                  <GrProjects />
-                  <Link>Projects</Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton>
-                  <BiCategoryAlt /> <Link>Categories</Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton>
-                  <FiUsers />
-                  <Link>Users</Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroup>
-
-          <SidebarGroup>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton>
-                  <GoDot />
-                  <Link>new cate</Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-
-              <SidebarMenuItem>
-                <SidebarMenuButton>
-                  <IoDocumentTextOutline />
-                  <Link to={RouteDocs}>Documentation</Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroup>
-        </SidebarContent>
-      </Sidebar>
+            <SidebarMenuItem>
+              <SidebarMenuButton>
+                <IoDocumentTextOutline />
+                <Link to={RouteDocs}>Documentation</Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
   );
 };
 
