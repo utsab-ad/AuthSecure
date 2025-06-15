c0fd2e252c225c73400b6c38dbd1caf47273d540
utsab-ad <utasbadhikari075@gmail.com>
2025-06-15 09:18:47 -0600

    progress added

--- backend/index.js
+++ backend/index.js
@@ -47,6 +47,18 @@ app.use("/news", NewsRoute);
 app.use("/hireme", HiremeRoute)
 app.use("/progress", ProgressRoute)
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin); 
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+
 //listining
 
 app.listen(PORT, () => {
